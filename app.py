2709aa28707dae12ba973e37996ee487f44be870
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-05-30 12:00:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,6 @@ def dashboard_page(df):
 
     # Sidebar filters
     regions = df['Region'].unique()
-    
     products = df['Product'].unique()
     
     date_min, date_max = df['Date'].min(), df['Date'].max()
