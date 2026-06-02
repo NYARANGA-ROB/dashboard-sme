cf2dad23526b417ec2a54ee8732e85d60e5cf920
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-06-02 16:45:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import xlsxwriter
 
 from src.data_loader import load_data
 from src.kpis import calculate_kpis
-
 from src.visuals import sales_by_region, age_distribution, gender_pie
 
 from src.insights import generate_insight
